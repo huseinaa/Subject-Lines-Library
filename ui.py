53a143fadd854ac5b48b66025e45d4e33ba2d159
Husein <husein.70821@hotmail.com>
2024-09-05 13:37:56 +0300

    update

--- ui.py
+++ ui.py
@@ -15,10 +15,11 @@ def insert_into_sheet(json_file, sheet_id, subject_line, data, row):
     # Use the credentials directly from the dictionary
     creds = Credentials.from_service_account_info(json_file_dict, scopes=scopes)
     client = gspread.authorize(creds)
+    st.text("Try")
     sheet = client.open_by_key(sheet_id)
     worksheet = sheet.get_worksheet(0)
 
-    st.text("Try")
+    st.text("Try22")
 
     try:
         # Insert subject line in the first cell of the row
