391bd154d23e2b96234f9e6a678b370d22bcb6b2
Husein <husein.70821@hotmail.com>
2024-09-05 15:43:25 +0300

    update

--- ui.py
+++ ui.py
@@ -1,48 +1,26 @@
+import json
 import gspread
 import streamlit as st
-import json
 from ast import literal_eval
 from SimplerLLM.language.llm import LLM, LLMProvider
 from google.oauth2.service_account import Credentials
 
-
 def insert_into_sheet(json_file, sheet_id, subject_line, data, row):
+    json_file_content = json_file.read()
+    json_file_dict = json.loads(json_file_content) 
+    
     scopes = ["https://www.googleapis.com/auth/spreadsheets"]
-
-    # Load the JSON content from the uploaded file
-    json_file_content = json_file.read()  # Read the file
-    json_file_dict = json.loads(json_file_content)  # Parse it as JSON
-
-    st.text(json_file_dict)  # Debugging output
-    st.text(type(json_file_dict))  # Check the type to ensure it's a dictionary
-
-    # Use the credentials directly from the dictionary
     creds = Credentials.from_service_account_info(json_file_dict, scopes=scopes)
-    st.text("Trying to authenticate with Google Sheets...")
-
     client = gspread.authorize(creds)
     sheet = client.open_by_key(sheet_id)
     worksheet = sheet.get_worksheet(0)
 
-    st.text("Authenticated and accessing worksheet...")
-
     try:
-        # Insert subject line in the first cell of the row
-        st.write(f"Updating cell ({row}, 1) with subject_line: {subject_line}")
-        worksheet.update_cell(row, 1, str(subject_line))  # Convert to string
-
-        # Insert other values if present in the data list
+        worksheet.update_cell(row, 1, str(subject_line))  
         if len(data) >= 3:
-            st.write(f"Updating cell ({row}, 2) with score: {data[0]}")
-            worksheet.update_cell(row, 2, str(data[0]))  # Score, as string
-
-            st.write(f"Updating cell ({row}, 3) with template: {data[1]}")
-            worksheet.update_cell(row, 3, str(data[1]))  # Template, as string
-
-            st.write(f"Updating cell ({row}, 4) with topic: {data[2]}")
-            worksheet.update_cell(row, 4, str(data[2]))  # Topic classification, as string
-
-        st.success(f"Data successfully inserted into row {row}.")
+            worksheet.update_cell(row, 2, str(data[0]))  # Score
+            worksheet.update_cell(row, 3, str(data[1]))  # Template
+            worksheet.update_cell(row, 4, str(data[2]))  # Topic classification
     except Exception as e:
         st.error(f"Error inserting data into Google Sheets: {e}")
 
@@ -107,7 +85,7 @@ def generate_response(subject_line: str):
 
     r_prompt = "Just return the following and nothing else: [\"87\", \"3 ways to [Achieve Goal] [Emoji]\", \"Informational\"]"
 
-    response = llm_instance.generate_response(prompt=r_prompt)
+    response = llm_instance.generate_response(prompt=u_prompt)
 
     return response
 
@@ -115,7 +93,7 @@ st.title('Subject Lines Automation')
 
 subject_line = st.text_input('Enter your email subject line:', '')
 row = st.text_input('Enter the row you want to edit:', '')
-file = st.file_uploader('Upload your Google service account key JSON file')
+file = st.file_uploader('Upload the Google service account key JSON file')
 
 if 'analyzed' not in st.session_state:
     st.session_state.analyzed = False
@@ -136,8 +114,6 @@ if st.session_state.analyzed:
         with col1:
             st.subheader('Result 1')
             result1_list = literal_eval(result1)
-            st.text(result1_list)  # Debugging output
-            st.text(type(result1_list))
             st.text_area('Output:', value=result1, height=None, disabled=True, key="Text_Area_1")
             if st.button('Add to Google Sheets.', key='save_to_sheet1'):
                 try:
